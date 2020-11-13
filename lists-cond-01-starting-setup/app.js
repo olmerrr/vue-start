f97aaecaabad741ec7dde55244dc32a331994430
Yaroslav <dombrovskiy91@gmail.com>
2020-11-13 13:26:26 +0200

    remove btn + remove list elem witch splice()

--- lists-cond-01-starting-setup/app.js
+++ lists-cond-01-starting-setup/app.js
@@ -12,6 +12,9 @@ const app = Vue.createApp({
   methods: {
     addGoal() {
       this.goals.push(this.enteredGoalValue)
+    },
+    removeGoal(idx) {
+      this.goals.splice(idx, 1)
     }
   }
 });
