027dc799f6b3537cb309ca9be5728d52b2845ef2
Yaroslav <dombrovskiy91@gmail.com>
2020-11-15 21:05:22 +0200

    add specialAtack

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -7,7 +7,7 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
-
+      currentRound: 0,
     };
   },
   computed: {
@@ -17,9 +17,13 @@ const app = Vue.createApp({
     playerBarStyles() {
       return {width: this.playerHealth + '%'};
     },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth = this.monsterHealth - attackValue;
       this.attackPlayer();
@@ -28,6 +32,12 @@ const app = Vue.createApp({
       const attackValue = getRandomValue(8, 15);
       this.playerHealth = this.playerHealth - attackValue;
     },
-  }
+    specialArrackMonster(){
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth = this.monsterHealth - attackValue;
+      this.attackPlayer();
+    }
+ }
 });
 app.mount('#game');
