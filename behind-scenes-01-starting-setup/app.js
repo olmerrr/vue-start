9bd64c53950e27e3b4dfce7ecc7e6873a1e8bfe8
Yaroslav <dombrovskiy91@gmail.com>
2020-11-16 22:42:00 +0200

    refs

--- behind-scenes-01-starting-setup/app.js
+++ behind-scenes-01-starting-setup/app.js
@@ -1,10 +1,4 @@
 const app = Vue.createApp({
-  template: `
-  <h2>How Vue Works</h2>
-  <input type="text" @input="saveInput">
-  <button @click="setText">Set Text</button>
-  <p>{{ message }}</p>
-  `,
   data() {
     return {
       currentUserInput: '',
@@ -16,7 +10,9 @@ const app = Vue.createApp({
       this.currentUserInput = event.target.value;
     },
     setText() {
-      this.message = this.currentUserInput;
+      // this.message = this.currentUserInput;
+      this.message = this.$refs.userText.value;
+      // console.dir(this.$refs.userText);
     },
   },
 });
