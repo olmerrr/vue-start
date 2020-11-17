109dca25bcef05c0cedb1f14699d282c9fefc7ce
Yaroslav <dombrovskiy91@gmail.com>
2020-11-17 17:35:35 +0200

    components 1step

--- cmp-intro-01-starting-setup/app.js
+++ cmp-intro-01-starting-setup/app.js
@@ -1,22 +1,46 @@
-const app  = Vue.createApp({
+const app = Vue.createApp({
+  data() {
+    return {
+      friends: [{
+          id: 1,
+          name: 'Wayne Mark Rooney',
+          phone: '124 22 02 02',
+          email: 'wayne_rooney@gmail.com'
+        },
+        {
+          id: 2,
+          name: 'Paul Aaron Scholes',
+          phone: '134 12 92 02',
+          email: 'striker001@gmail.com'
+        },
+      ]
+    }
+  }
+});
+
+app.component('friend-contact', {
+  template: `
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? 'Hide' : 'Show'}} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li><strong>Phone:</strong>{{ friend.phone }}</li>
+      <li><strong>Email:</strong> {{ friend.email }}</li>
+    </ul>
+  </li>
+`,
   data() {
     return {
       detailsAreVisible: false,
-      friends : [
-      {
+      friend: {
         id: 1,
         name: 'Wayne Mark Rooney',
         phone: '124 22 02 02',
         email: 'wayne_rooney@gmail.com'
       },
-      {
-        id: 2,
-        name: 'Paul Aaron Scholes',
-        phone: '134 12 92 02',
-        email: 'striker001@gmail.com'
-      },
-    ]
-    }
+    };
   },
   methods: {
     toggleDetails() {
